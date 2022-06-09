a5fcd72854e638aca3aed66aa107ba49df5ce26f
loksblnine <illya01@ukr.net>
2022-06-09 16:28:33 +0300

    delete: trash

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const token = process.env.API_TELEGRAM_HTTP_TOKEN;
 const bot = new TelegramAPI(token, {polling: true});
 
 const start = () => {
-  bot.onText(/\/echo/, (msg) => {
-    const {text, chat} = msg;
-    const chatId = chat.id;
-    bot.sendMessage(chatId, `haha ez ${text}`);
-  });
 
   bot.on('message', (message) => {
     console.log(message);
