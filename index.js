99c8a142d89637339fc486d1ede356c6f85c0c63
loksblnine <illya01@ukr.net>
2022-06-09 16:24:06 +0300

    feat: `echo` command

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const token = process.env.API_TELEGRAM_HTTP_TOKEN;
 const bot = new TelegramAPI(token, {polling: true});
 
 const start = () => {
+  bot.onText(/\/echo/, (msg) => {
+    const {text, chat} = msg;
+    const chatId = chat.id;
+    bot.sendMessage(chatId, `haha ez ${text}`);
+  });
 
   bot.on('new_chat_members', (msg) => {
     const {chat} = msg;
