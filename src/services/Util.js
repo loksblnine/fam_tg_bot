94fa15edbf549e72e774a92498dcc67e77ad287c
loksblnine <illya01@ukr.net>
2022-06-15 00:32:35 +0300

    remove: trash

--- src/services/Util.js
+++ src/services/Util.js
@@ -7,21 +7,24 @@ const isUserInDatabase = (userId) => {
 const filterNonExist = (usersArray) => {
   let array = [];
 
-  usersArray.forEach(async (user, id, array) => {
-    console.log(isUserInDatabase(user.id));
+  usersArray.forEach((user, id, array) => {
     if (!isUserInDatabase(user.id)) {
-      await UserController.createUser(user.username, user.id);
-      array.push(user);
+      console.log(5432, true);
+      UserController.createUser(user.username, user.id)
+        .then(() => {
+          console.log(111, user);
+          array.push(user);
+        });
     }
   });
-
+  console.log("final array ", array);
   return array;
 };
 
 const concatUserAppeal = (usersArray) => {
   let string = "";
 
-  usersArray.forEach(async (user, id, array) => {
+  usersArray.forEach((user, id, array) => {
     string += `[${user.username}](tg://user?id=${user.id})`;
     if (array.length <= id - 1) {
       string += ', ';
